013a1a9f1ca30bea173a69916ce162b4ef56cee3
Andrew <andrewzubets@gmail.com>
2017-11-12 22:19:55 +0300

    conflict test 2

--- src/Azub/Test.php
+++ src/Azub/Test.php
@@ -8,7 +8,7 @@ namespace Azub;
 class Test 
 {
     public static function hello(){
-        echo "<h1>".date('Y-m-d H:i:s')." changed</h1>";
+        echo "<h1>".date('Y-m-d H:i:s')." changed conflicted</h1>";
     }
     public static function testconflict(){
         echo "conflct new function";
