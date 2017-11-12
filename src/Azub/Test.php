7cc6d38d1c7b24da830165282c35eaa20fbb0a98
Andrew <andrewzubets@gmail.com>
2017-11-12 21:58:13 +0300

    conflict test commit

--- src/Azub/Test.php
+++ src/Azub/Test.php
@@ -8,7 +8,10 @@ namespace Azub;
 class Test 
 {
     public static function hello(){
-        echo "<h1>".date('Y-m-d H:i:s')."</h1>";
+        echo "<h1>".date('Y-m-d H:i:s')." changed</h1>";
+    }
+    public static function testconflict(){
+        echo "conflct new function";
     }
 }
 
