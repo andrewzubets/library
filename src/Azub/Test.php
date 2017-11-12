69145cc5348e38c283f49dfa21888e9b8c2044c4
Andrew <andrewzubets@gmail.com>
2017-11-12 22:33:58 +0300

    test merge

--- src/Azub/Test.php
+++ src/Azub/Test.php
@@ -8,7 +8,7 @@ namespace Azub;
 class Test 
 {
     public static function hello(){
-        echo "<h1>".date('Y-m-d H:i:s')." changed master</h1>";
+        echo "<h1>".date('Y-m-d H:i:s')." changed master 22</h1>";
     }
     public static function testconflict(){
         echo "conflct new function";
